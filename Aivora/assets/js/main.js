6dd840a7eade62b528f1ca8fe324ece23ff14604
Mihirvarude9 <mvarude09@gmail.com>
2025-12-24 01:16:49 +0530

    Change marquee to use duration parameter for faster scrolling

--- Aivora/assets/js/main.js
+++ Aivora/assets/js/main.js
@@ -538,7 +538,7 @@
 	= marquee
 	-------------------------------------------*/
 	$('.marquee-left').marquee({
-		speed: 50,
+		duration: 5000,
 		gap: 0,
 		delayBeforeStart: 0,
 		direction: 'left',
@@ -547,7 +547,7 @@
 		startVisible: true,
 	});	
 	$('.marquee-right').marquee({
-		speed: 50,
+		duration: 5000,
 		gap: 0,
 		delayBeforeStart: 0,
 		direction: 'right',
