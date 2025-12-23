239b2bee96c3efdfe88761bd16e4db55db4f1547
Mihirvarude9 <mvarude09@gmail.com>
2025-12-23 21:44:00 +0530

    Increase brand logo marquee scrolling speed

--- Aivora/assets/js/main.js
+++ Aivora/assets/js/main.js
@@ -538,7 +538,7 @@
 	= marquee
 	-------------------------------------------*/
 	$('.marquee-left').marquee({
-		speed: 20,
+		speed: 10,
 		gap: 0,
 		delayBeforeStart: 0,
 		direction: 'left',
@@ -547,7 +547,7 @@
 		startVisible: true,
 	});	
 	$('.marquee-right').marquee({
-		speed: 20,
+		speed: 10,
 		gap: 0,
 		delayBeforeStart: 0,
 		direction: 'right',
