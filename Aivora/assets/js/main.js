a997755de099aa22724c3fdeebc0381ffc690ef2
Mihirvarude9 <mvarude09@gmail.com>
2025-12-24 01:20:42 +0530

    Set marquee duration to 1000ms for super fast scrolling

--- Aivora/assets/js/main.js
+++ Aivora/assets/js/main.js
@@ -538,7 +538,7 @@
 	= marquee
 	-------------------------------------------*/
 	$('.marquee-left').marquee({
-		speed: 100,
+		duration: 1000,
 		gap: 0,
 		delayBeforeStart: 0,
 		direction: 'left',
@@ -547,7 +547,7 @@
 		startVisible: true,
 	});	
 	$('.marquee-right').marquee({
-		speed: 100,
+		duration: 1000,
 		gap: 0,
 		delayBeforeStart: 0,
 		direction: 'right',
