67e36680039318ee9fa123405e46af490d4123f0
Mihirvarude9 <mvarude09@gmail.com>
2025-12-24 01:20:52 +0530

    Set marquee to ultra fast 500ms duration

--- Aivora/assets/js/main.js
+++ Aivora/assets/js/main.js
@@ -538,7 +538,7 @@
 	= marquee
 	-------------------------------------------*/
 	$('.marquee-left').marquee({
-		duration: 1000,
+		duration: 500,
 		gap: 0,
 		delayBeforeStart: 0,
 		direction: 'left',
@@ -547,7 +547,7 @@
 		startVisible: true,
 	});	
 	$('.marquee-right').marquee({
-		duration: 1000,
+		duration: 500,
 		gap: 0,
 		delayBeforeStart: 0,
 		direction: 'right',
